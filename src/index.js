cb2a4026bda6d700e8eae5c98e19158ca8694ce0
inord777dev <inord777dev@gmail.com>
2021-08-03 15:23:12 +0300

    init: start and complite

--- src/index.js
+++ src/index.js
@@ -38,7 +38,22 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let letters = expr.match(/.{1,10}/g);
+    let message = '';
+    letters.forEach(letter => {
+        if (letter == "**********") {
+            message += " ";
+        } else {
+            let sign = "";
+            for (let index = 0; index < 5; index++ ) {
+                if (letter[index*2] != "0" || letter[index*2 + 1] != "0") {
+                    sign += letter[index*2] == "1" && letter[index*2 + 1] == "1" ? '-' : '.';
+                }
+            }
+            message += MORSE_TABLE[sign];
+        }
+    }); 
+    return message;
 }
 
 module.exports = {
